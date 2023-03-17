969492dd07ade543c821d7c422ae5d8b432cfe12
yazminher <119539077+yazminher@users.noreply.github.com>
2023-03-17 09:42:16 -0700

    Initial commit

---
+++ app/src/main/java/com/example/base64exameng2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.base64exameng2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
